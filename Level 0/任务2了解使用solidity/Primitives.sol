13f18202a750e5b8cae47c919dde457c5dbff5c0
hujf <50021327+hujianfei2@users.noreply.github.com>
2025-02-17 20:35:00 +0800

    Update Primitives.sol

--- Level 0/任务2了解使用solidity/Primitives.sol
+++ Level 0/任务2了解使用solidity/Primitives.sol
@@ -7,19 +7,16 @@ contract Primitives {
     
     uint8 public u8 = 1;
     uint256 public u256 = 456;
-    uint256 public u = 123; // uint is an alias for uint256
+    uint256 public u = 123; 
 
-   
     int8 public i8 = -1;
     int256 public i256 = 456;
-    int256 public i = -123; // int is same as int256
-
+    int256 public i = -123; 
 
     int256 public minInt = type(int256).min;
     int256 public maxInt = type(int256).max;
 
     address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-
 
     bytes1 a = 0xb5; 
     bytes1 b = 0x56; 
