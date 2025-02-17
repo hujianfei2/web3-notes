69ab934ea6343a3c5c9d91978a0ece112944e100
hujf <50021327+hujianfei2@users.noreply.github.com>
2025-02-17 20:30:55 +0800

    Update and rename Hellowrold.md to Hellowrold.sol

--- Level 0/任务2了解使用solidity/Hellowrold.sol
+++ Level 0/任务2了解使用solidity/Hellowrold.sol
@@ -5,5 +5,3 @@ pragma solidity 0.8.26;
 contract HelloWorld {
     string public greet = "Hello World!";
 }
-
-![image](https://github.com/user-attachments/assets/67811530-2376-4bdd-87bd-fa919faccf9f)
